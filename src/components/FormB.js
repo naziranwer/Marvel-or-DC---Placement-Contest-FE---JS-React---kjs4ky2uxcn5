02fd8a70e47980463234d37cb11af694301447e0
naziranwer <122814116+naziranwer@users.noreply.github.com>
2023-08-27 18:57:32 +0530

    Update FormB.js

--- src/components/FormB.js
+++ src/components/FormB.js
@@ -1,45 +1,47 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import Summary from "./Summary";
 function FormB({ onSubmit, age }) {
-  const [selectedShow, setSelectedShow] = useState("");
+    const [marvelShows, setMarvelShows] = useState("");
+    const [formData, setFormData] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (selectedShow) {
-      onSubmit(selectedShow);
-    }
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (marvelShows != "") {
+            setFormData({
+                type: "Form B",
+                age: age,
+                show: marvelShows,
+            });
+        }
+    };
 
-  const handleShowChange = (event) => {
-    setSelectedShow(event.target.value);
-  };
-
-  return (
-    <form id="marvel" onSubmit={handleSubmit}>
-      <h2>Form B</h2>
-      <label>
-        Select Marvel Shows:
-        <select value={selectedShow} onChange={handleShowChange}>
-          <option value="">--Select--</option>
-          <option value="WandaVision">WandaVision</option>
-          <option value="The Falcon and the Winter Soldier">
-            The Falcon and the Winter Soldier
-          </option>
-          <option value="Loki">Loki</option>
-          <option value="What If...?">What If...?</option>
-        </select>
-      </label>
-      <br />
-      <label>
-        Enter your age:
-        <input type="number" value={age} disabled />
-      </label>
-      <br />
-      <button id="submit-marvel" type="submit" disabled={!selectedShow}>
-        Submit
-      </button>
-    </form>
-  );
+    return formData === "" ? (
+        <form id="marvel" onSubmit={handleSubmit}>
+            <h2>Form B</h2>
+            <label>
+                Select Marvel Shows:
+                <select onChange={(e) => setMarvelShows(e.target.value)}>
+                    <option value="">--Select--</option>
+                    <option value="WandaVision">WandaVision</option>
+                    <option value="The Falcon and the Winter Soldier">
+                        The Falcon and the Winter Soldier
+                    </option>
+                    <option value="Loki">Loki</option>
+                    <option value="What If...?">What If...?</option>
+                </select>
+            </label>
+            <br />
+            <label>
+                Enter your age:
+                <input type="number" value={age} disabled />
+            </label>
+            <br />
+            <button id="submit-marvel" type="submit">
+                Submit
+            </button>
+        </form>
+    ) : (
+        <Summary formData={formData} />
+    );
 }
-
 export default FormB;
