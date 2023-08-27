0311a4785d694df1fe6aa623c9f1cf05566fd6f6
naziranwer <122814116+naziranwer@users.noreply.github.com>
2023-08-27 18:56:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,87 +1,64 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FormA from "./FormA";
 import FormB from "./FormB";
 import Summary from "./Summary";
 
 const App = () => {
-  const [step, setStep] = useState(1);
-  const [formData, setFormData] = useState({});
-  const [age, setAge] = useState("");
-  const [formType, setFormType] = useState("");
+    const [step, setStep] = useState(1);
+    const [formData, setFormData] = useState({});
+    const [age, setAge] = useState("");
 
-  const handleAgeChange = (event) => {
-    const newAge = event.target.value;
-    setAge(newAge);
-  };
-
-  const handleFormTypeChange = (event) => {
-    const newFormType = event.target.value;
-    setFormType(newFormType);
-    setStep(1); // Reset step to 1 when form type changes
-  };
-
-  const handleSubmitForm = (selectedShow) => {
-    setFormData({
-      age: age,
-      formType: formType === "Form A" ? "Form A" : "Form B",
-      show: selectedShow,
-    });
-    setStep(4); // Move to summary step
-  };
-
-  const handleStartOver = () => {
-    setAge("");
-    setFormType("");
-    setFormData({});
-    setStep(1);
-  };
-
-  return (
-    <div>
-      {step === 1 && (
-        <div id="start-page">
-          <h1>Step 1: Select Form Type and Enter Age</h1>
-          <label>
-            Enter your age:
-            <input type="number" value={age} onChange={handleAgeChange} />
-          </label>
-          <br />
-          <label>
-            Select Form Type:
-            <select value={formType} onChange={handleFormTypeChange}>
-              <option value="">--Select--</option>
-              <option value="Form A">Form A</option>
-              <option value="Form B">Form B</option>
-            </select>
-          </label>
-          <br />
-        </div>
-      )}
-      {step === 2 && formType === "Form A" && (
+    return (
         <div>
-          <FormA age={age} onSubmit={handleSubmitForm} />
+            {(step === 1 || !age) && (
+                <div id="start-page">
+                    <h1>Step 1: Select Form Type and Enter Age</h1>
+                    <label>
+                        Enter your age:
+                        <input
+                            value={age}
+                            type="number"
+                            onChange={(e) => setAge(e.target.value)}
+                        />
+                    </label>
+                    <br />
+                    <label>
+                        Select Form Type:
+                        <select
+                            onChange={(e) => setStep(parseInt(e.target.value))}
+                        >
+                            <option value={1}>--Select--</option>
+                            <option value={2}>Form A</option>
+                            <option value={3}>Form B</option>
+                        </select>
+                    </label>
+                    <br />
+                </div>
+            )}
+            {step === 2 && (
+                <div>
+                    <FormA age={age} />
+                </div>
+            )}
+            {step === 3 && (
+                <div>
+                    <FormB age={age} />
+                </div>
+            )}
+            {(step === 2 || step === 3) && age ? (
+                <button id="go-back" onClick={() => setStep(1)}>
+                    Go Back
+                </button>
+            ) : null}
+
+            {step === 4 && (
+                <div>
+                    <Summary />
+                    <button id="start-over">Start Over</button>
+                </div>
+            )}
         </div>
-      )}
-      {step === 3 && formType === "Form B" && (
-        <div>
-          <FormB age={age} onSubmit={handleSubmitForm} />
-        </div>
-      )}
-      {(step === 2 || step === 3) && age ? (
-        <button id="go-back" onClick={() => setStep(1)}>
-          Go Back
-        </button>
-      ) : null}
-      {step === 4 && (
-        <div>
-          <Summary formData={formData} />
-          <button id="start-over" onClick={handleStartOver}>
-            Start Over
-          </button>
-        </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default App;
