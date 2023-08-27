4b19dd6d7a3ee41aec4e8408bfe65211fa5409eb
naziranwer <122814116+naziranwer@users.noreply.github.com>
2023-08-27 18:58:06 +0530

    Update Summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,14 +1,24 @@
-import React from "react";
-
+import React, { useState } from "react";
 function Summary({ formData }) {
-  return (
-    <div id="summary">
-      <h2>Summary</h2>
-      <p>Form Type: {formData.formType}</p>
-      <p>Age: {formData.age}</p>
-      <p>Show Name: {formData.show}</p>
-    </div>
-  );
-}
+    return (
+        <div id="summary">
+            <h2>Summary</h2>
+            <p>Form Type: {formData.type}</p>
+            <p>Age: {formData.age}</p>
 
+            {formData.type === "FormA" ? (
+                <p>DC Shows: {formData.show}</p>
+            ) : (
+                <p>Marvel Shows: {formData.show}</p>
+            )}
+
+            <button
+                id="start-over"
+                onClick={() => window.location.reload(true)}
+            >
+                Start Over
+            </button>
+        </div>
+    );
+}
 export default Summary;
